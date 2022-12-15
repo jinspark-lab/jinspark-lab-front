7486d4a88421f6ba382a039f2553b281dcf7c92a
jinspark-lab <jinsangp@gmail.com>
2022-12-15 09:37:18 -0500

    [FE] Change SkillBadge UI

--- src/app/AppMenu.js
+++ src/app/AppMenu.js
@@ -58,12 +58,6 @@ const AppMenu = () => {
                         </ul>
                     </div>
                 </div>
-                <div className='col-4'>
-                    <form className="d-flex">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                </div>
             </nav>
         }
         </div>
--- src/components/home/SkillBadge.js
+++ src/components/home/SkillBadge.js
@@ -3,6 +3,14 @@ import '../../styles/SkillBadge.css';
 
 // Single Item represent text
 const SkillBadge = ({ name, experience, proficiency }) => {
+    const SKILL_PROFICIENCY_VALUE_MAP = {
+        '1': 'BEGINNER',
+        '2': 'GOOD',
+        '3': 'ADVANCED',
+        '4': 'PROFESSIONAL',
+        '5': 'EXPERT'
+    };
+
     return (
         <div className="skill-box">
             <button className="skill-badge">
@@ -10,7 +18,7 @@ const SkillBadge = ({ name, experience, proficiency }) => {
                 {name}
                 </span>
                 <span className="skill-year">{experience} year</span>
-                <span className="skill-proficiency">{proficiency}</span>
+                <span className="skill-proficiency">{SKILL_PROFICIENCY_VALUE_MAP[proficiency]}</span>
             </button>
         </div>
     );
