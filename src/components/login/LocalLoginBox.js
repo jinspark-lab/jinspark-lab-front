0034f7546779ff55ccd08cac2443269a27958592
jinspark-lab <jinsangp@gmail.com>
2023-02-27 11:32:17 -0500

    [FE] Add comment

--- src/components/login/LocalLoginBox.js
+++ src/components/login/LocalLoginBox.js
@@ -1,6 +1,5 @@
 import React, { useCallback, useEffect } from 'react';
 import api from '../../module/Api';
-import cookie from '../../module/Cookie';
 import '../../styles/Login.css';
 
 const LocalLoginBox = ({ loginHandler }) => { 
